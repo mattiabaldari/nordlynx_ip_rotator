88b4d7d5867a75d81d0be3f01c38a8ea61fc2842
Mattia Baldari <mattia.baldari@kraken.tech>
2025-06-07 14:58:23 +0200

    Make sure to check the connection works successfully

--- root/root/restart_wireguard.sh
+++ root/root/restart_wireguard.sh
@@ -3,5 +3,15 @@
 
 # Kill the wireguard service process (s6-overlay will automatically restart it)
 s6-svc -k /var/run/service/wireguard
+sleep 10
+
+# Check if curl ifconfig.me runs successfully
+if ! curl -s --max-time 10 ifconfig.me > /dev/null; then
+  echo "[$(date -Iseconds)] Network check failed, waiting 60 seconds before retrying restart"
+  sleep 60
+
+  # Kill the wireguard service process (s6-overlay will automatically restart it)
+  s6-svc -k /var/run/service/wireguard
+fi
 
 echo "[$(date -Iseconds)] Wireguard service restarted"
